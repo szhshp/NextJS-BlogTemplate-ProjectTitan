c4f0c5da964ec4c4c54ba7bcd36f23f458f0ca6a
szh-hp-laptop <24031shp@sina.com>
2020-07-30 23:15:14 +0800

    Fixed: wrong post link

--- data/jumbotron.ts
+++ data/jumbotron.ts
@@ -3,8 +3,14 @@ export const backgroundPicture = "https://s1.ax1x.com/2020/07/29/aZVfeK.md.jpg";
 export const buttonSet = [
   [
     /* Want to update the button label? check locale.ts */
-    { label: "What's New", link: "/tech/2020/07/27/MainDocument#what-s-new-in-project-titan-" },
-    { label: "Github", link: "https://github.com/szhielelp/NextJS-BlogTemplate-ProjectTitan" },
+    {
+      label: "What's New",
+      link: "tech/2020/07/27/Document-Main#what-s-new-in-project-titan-",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/szhielelp/NextJS-BlogTemplate-ProjectTitan",
+    },
     { label: "Demo Blog", link: "https://szhshp.org" },
   ],
 ];
